72547105c2fd14c2f5532b74b8ae020d3ac1648d
Lina <lina.bamboo@gmail.com>
2018-05-02 13:29:58 +0300

    handle snapshot when it is in pending status

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -18,6 +18,11 @@ def filter_instances(project):
 
     return instances
 
+#check snapshot is pending or not
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 #nest clickgroups
 @click.group()
 def cli():
@@ -103,6 +108,10 @@ def create_snapshots(project):
         i.wait_until_stopped()
 
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print('Skipping {0}, snapshot already in progress.'.format(v.id))
+                continue
+                
             print('Creating snapshot of {0}'.format(v.id))
             v.create_snapshot(Description='Created by SnapshotAnalyzer_Script')
 
